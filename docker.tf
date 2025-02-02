adcf68e47db9a0475e192c86ecf78c0dbeb5b219
Joel Arias Cordova <joel_arias@Joels-MacBook-Pro.local>
2025-02-02 00:00:44 -0600

    first commit

--- docker.tf
+++ docker.tf
@@ -11,33 +11,33 @@ resource "aws_instance" "database" {
   }
 }
 
-resource "aws_instance" "docker_backEnd" {
+resource "aws_instance" "docker_back_end" {
   ami           = "ami-07d2649d67dbe8900"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Docker-BackEnd"
+    Name = "docker-back-end"
   }
 }
 
-resource "aws_instance" "docker_frontEnd" {
+resource "aws_instance" "docker_front_end" {
   ami           = "ami-07d2649d67dbe8900"
   instance_type = "t2.medium"
 
   tags = {
-    Name = "Docker-FrontEnd"
+    Name = "docker-front-end"
   }
 }
 
 output "database_public_ip" {
-  value = aws_instance.database
+  value = aws_instance.database.public_ip
 }
 
-output "docker_backEnd_public_ip" {
-  value = aws_instance.docker_backEnd.public_ip
+output "docker_back_end_public_ip" {
+  value = aws_instance.docker_back_end.public_ip
 }
 
-output "docker_frontEnd_public_ip" {
-  value = aws_instance.docker_frontEnd.public_ip
+output "docker_front_end_public_ip" {
+  value = aws_instance.docker_front_end.public_ip
 }
 
