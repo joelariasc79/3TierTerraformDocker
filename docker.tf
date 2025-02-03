462f94423a9aca9df2a60862b419698a5d890c95
Joel Arias Cordova <joel_arias@Joels-MacBook-Pro.local>
2025-02-02 20:24:18 -0600

    first commit

--- docker.tf
+++ docker.tf
@@ -170,3 +170,15 @@ output "docker_front_end_public_ip" {
   value = aws_instance.docker_front_end.public_ip
 }
 
+
+output "database_private_ip" {
+  value = aws_instance.database.private_ip
+}
+
+# output "docker_back_end_private_ip" {
+#   value = aws_instance.docker_back_end.private_ip
+# }
+#
+# output "docker_front_end_private_ip" {
+#   value = aws_instance.docker_front_end.public_ip
+# }
